44b0e8c1aab902011a372ddaec3697ed3eed7183
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2023-03-03 21:30:07 -0500

    reboot

--- .next/server/font-loader-manifest.js
+++
@@ -1,8 +0,0 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {},
-  "app": {
-    "D:\\PROJECT\\ECOMERCE\\next-ecomerce\\react-ecomerce\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
-      "static/media/2aaf0723e720e8b9.p.woff2"
-    ]
-  }
-}
